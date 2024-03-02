24a7c65e54388dc85fa0dea570167e1ae6079c7f
ALBERT IMASUEN <studentxapp@gmail.com>
2024-03-02 11:38:27 -0500

    Adding some functions

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Link from "next/link";
 // import DesktopLogo from "../../airbnb-desktop.png";
-import MobileLogo from "@/public/airbnb-mobile.webp";
-import DesktopLogo from "@/public/airbnb-desktop.png"
+import MobileLogo from "@/public/logo.svg";
+import DesktopLogo from "@/public/logo.svg"
 import { UserNav } from "./useNav";
 import { SearchModalCompnent } from "./SearchComponent";
 
 export function Navbar () {
+
+  //
     return (
         <nav className="w-full border-b">
         <div className="flex items-center justify-between container mx-auto px-5 lg:px-10 py-5">
@@ -14,7 +16,7 @@ export function Navbar () {
             <Image
               src={DesktopLogo}
               alt="Desktop Logo"
-              className="w-32 hidden lg:block"
+              className="w-12 hidden lg:block"
             />
   
             <Image
